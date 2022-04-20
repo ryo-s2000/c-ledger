40c739cb619dad7dabfee2eedc9cb617b302c36c
Ryo Suzuki <ryo.s12330@gmail.com>
2022-04-20 15:13:55 +0900

    fix width

--- src/resources/js/pages/ledger/IndexComponent.vue
+++ src/resources/js/pages/ledger/IndexComponent.vue
@@ -1,16 +1,6 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        aaa
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div>
+        aaaa
     </div>
 </template>
 
